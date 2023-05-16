0df6d5dfd867669731a83cd784256fe96fd2ddde
Terry-Koo <xpflxhfl94@naver.com>
2023-05-17 00:09:09 +0900

    first commit

---
+++ Simple/SimpleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleApp.swift
+//  Simple
+//
+//  Created by Terry Koo on 2023/05/17.
+//
+
+import SwiftUI
+
+@main
+struct SimpleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
